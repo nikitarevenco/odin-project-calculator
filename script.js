418c6fc2dba1ea3d5ddac63a25191d29ed147055
Nikita Revenco <154856872+nikitarevenco@users.noreply.github.com>
2024-01-10 16:05:40 +0000

    Add basic calculator functionality
    Only "!" and "9" works for now

--- script.js
+++ script.js
@@ -1,5 +1,56 @@
-const para = document.querySelector(".abc");
-para.textContent = ``;
+const inputField = document.querySelector(".input-field");
+const btnNine = document.querySelector(".nine");
+const btnEight = document.querySelector(".eight");
+const btnSeven = document.querySelector(".seven");
+const btnSix = document.querySelector(".six");
+const btnFive = document.querySelector(".five");
+const btnFour = document.querySelector(".four");
+const btnThree = document.querySelector(".three");
+const btnTwo = document.querySelector(".two");
+const btnOne = document.querySelector(".one");
+const btnZero = document.querySelector(".zero");
+const btnDel = document.querySelector(".del");
+const btnAc = document.querySelector(".ac");
+const btnReverseSign = document.querySelector(".reverse-sign");
+const btnEquals = document.querySelector(".equals");
+const btnPeriod = document.querySelector(".period");
+const btnFactorial = document.querySelector(".factorial");
+const btnSin = document.querySelector(".sin");
+const btnCos = document.querySelector(".cos");
+const btnTan = document.querySelector(".tan");
+const btnAdd = document.querySelector(".add");
+const btnSubtract = document.querySelector(".subtract");
+const btnDivide = document.querySelector(".divide");
+const btnMultiply = document.querySelector(".multiply");
+const btnPower = document.querySelector(".power");
+
+let input1 = 0;
+let input2 = 0;
+let operator;
+let operatorSingle;
+
+function calculator() {
+  input1 = inputField.textContent;
+  input2 = input1;
+
+  if (operator && operatorSingle === true) {
+    if (operator === "factorial") {
+      inputField.textContent = factorial(input1);
+    }
+  }
+}
+
+btnFactorial.addEventListener("click", function () {
+  operator = "factorial";
+  operatorSingle = true;
+  calculator();
+});
+
+btnNine.addEventListener("click", function () {
+  inputField.textContent += "9";
+});
+
+// At first, I will only add support for "9" and "add"
 
 function factorial(input) {
   if (input === 0) {
@@ -7,7 +58,6 @@ function factorial(input) {
   } else if (input % 1 !== 0) {
     return "ERROR";
   }
-
   storedInput = input;
   for (let i = input - 1; i > 0; i--) {
     storedInput *= i;
